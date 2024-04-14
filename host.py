b9fca10f9b1e5fed964220fb716ca85b619b8210
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-04-14 14:33:53 +0530

    host.py

--- host.py
+++ host.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('C:/Users/shubh/machine_learning_project/Multiple_disease_prediction/multiple_disease_prediction/svm_diabetes_model.sav', 'rb'))
+loaded_model = pickle.load(open('svm_diabetes_model.sav', 'rb'))
 
 #scaler = StandardScaler()
 
